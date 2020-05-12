1df40f5af343958e2c1bfed44ff838bdad8d4b07
Stephanie Cheney <steph@scheneyphotography.com>
2020-05-11 22:38:03 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
-class Pirate
+class Pirate 
+    attr_accessor :name, :weight, :height 
+    PIRATES =[]
+    
+    def initialize(input)
+        @name=input[:name] 
+        @weight=input[:weight]
+        @height=input[:height] 
+        PIRATES << self 
+    end
+    
+    def self.all 
+        PIRATES
+    end 
+
+    
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@ships =[]
+    
+    def initialize(input)
+        @name=input[:name] 
+        @type=input[:type]
+        @booty=input[:booty]
+        @@ships << self 
+    end
+    
+    def self.all 
+        @@ships
+    end 
+
+    def self.clear
+        @@ships= []
+    end 
 end
